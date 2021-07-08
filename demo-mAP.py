243c17a3c34ccccbe3aed449b905b50d39737315
chenghanc <chenghan.chung@rwth-aachen.de>
2021-07-08 21:17:16 +0800

    add demo-mAP

---
+++ demo-mAP.py
@@ -0,0 +1,22 @@
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+annType = 'bbox'
+
+anno_file = 'darknet_valid_gt_baby.json'
+results_json = 'darknet_pred_baby.json'
+
+cocoGt=COCO(anno_file)
+cocoDt=cocoGt.loadRes(results_json)
+
+imgIds=sorted(cocoDt.getImgIds())
+len(imgIds)
+
+# running evaluation
+cocoEval = COCOeval(cocoGt,cocoDt,annType)
+cocoEval.params.imgIds = imgIds
+cocoEval.evaluate()
+cocoEval.accumulate()
+cocoEval.summarize()
+
+print(cocoEval.stats)
