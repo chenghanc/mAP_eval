47080e272fecc58b22ac2a58ee7bca1b1fb00565
chenghanc <chenghan.chung@rwth-aachen.de>
2021-07-17 18:56:00 +0800

    update

--- demo-mAP.py
+++ demo-mAP.py
@@ -65,6 +65,19 @@ else:
 print(" -----")
 print(" All Categories : %.2f" % (all_precision.mean() * 100) + "\n" + " ***********************" + "\n")
 
+print(" \n" + " AP@[ IoU=0.50:0.95 ]" + " (%)")
+print(" ***********************")
+all_precision2 = cocoEval.eval['precision']
+num_classes = 6
+avg_ap = 0.0
+for i in range(0, num_classes):
+    # 0:all 1:small 2:medium 3:large
+    s = all_precision2[:,:,i,0,2]
+    print(' Category :  {0}  : {1:.2f}  -  {2}'.format(i,s.mean() * 100,names[i]))
+    avg_ap += s.mean() * 100
+print(" -----")
+print(" All Categories : {:.2f}".format(avg_ap / num_classes) + "\n" + " ***********************" + "\n")
+
 plt.plot(x, all_precision.mean(1), linewidth=3, color='blue', label='all classes %.3f mAP@0.50' % all_precision.mean())
 plt.title('PR Curve: mAP@0.50 =  %.3f' % all_precision.mean())
 plt.ylabel("Precision")
